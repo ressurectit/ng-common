9ab551238cce82f3d691a6663811084b5c8792d1
kukjevov <kukjevov@gmail.com>
2021-10-25 08:18:51 +0200

    INT: added opts for invalidate on change

--- forms/src/misc/formModel/decorators/invalidateOnChange.decorator.ts
+++ forms/src/misc/formModel/decorators/invalidateOnChange.decorator.ts
@@ -1,13 +1,28 @@
 import {FormGroup} from '@angular/forms';
+import {isBlank} from '@jscrpt/common';
 
 import {Validator} from './validator.decorator';
 
 /**
  * Invalidates `FormControl` when current `FormControl` values changes
- * @param property - Name or array of `FormControl` names that will be invalidated
+ * @param property - Name of `TModel` control or array of these properties to be invalidated on change
+ * @param opts - Configuration options determine how the control propagates changes and emits events on sibling controls
+ * after updates and validity checks are applied.
+ * * `onlySelf`: When true, only update this control. When false or not supplied,
+ * update all direct ancestors. Default is false.
+ * * `emitEvent`: When true or not supplied (the default), both the `statusChanges` and
+ * `valueChanges`
+ * observables emit events with the latest status and value when the control is updated.
+ * When false, no events are emitted.
+ * * `markAsTouched` calls also `markAsTouched` on sibling control
+ * * `markAsDirty` calls also `markAsDirty` on sibling control
  */
-export function InvalidateOnChange<TModel = any>(property: Extract<keyof TModel, string>|Extract<keyof TModel, string>[]): PropertyDecorator
+export function InvalidateOnChange<TModel = any>(property: Extract<keyof TModel, string>|Extract<keyof TModel, string>[],
+                                                 opts?: {onlySelf?: boolean, emitEvent?: boolean, markAsTouched?: boolean, markAsDirty?: boolean}): PropertyDecorator
 {
+    const emitEvent: boolean = isBlank(opts?.emitEvent) || opts?.emitEvent === true;
+    const onlySelf: boolean = opts?.onlySelf ?? false;
+
     return Validator(control =>
     {
         if(!Array.isArray(property))
@@ -21,7 +36,17 @@ export function InvalidateOnChange<TModel = any>(property: Extract<keyof TModel,
         {
             property.forEach(prop =>
             {
-                parent.get(prop)?.updateValueAndValidity();
+                parent.get(prop)?.updateValueAndValidity({emitEvent, onlySelf});
+
+                if(opts?.markAsDirty)
+                {
+                    parent.get(prop)?.markAsDirty({onlySelf});
+                }
+
+                if(opts?.markAsTouched)
+                {
+                    parent.get(prop)?.markAsTouched({onlySelf});
+                }
             });
         }
 
