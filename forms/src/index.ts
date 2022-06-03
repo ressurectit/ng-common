d7ab1ff492b87dd9cce20b104cd821f8ed77779b
kukjevov <kukjevov@gmail.com>
2022-06-03 10:01:02 +0200

    INT: added forgotten export

--- forms/src/index.ts
+++ forms/src/index.ts
@@ -7,9 +7,10 @@ export * from './directives/numberInput/numberInputControlValueAccessor.directiv
 export * from './directives/hasError/hasError.directive';
 export * from './directives/groupHasError/groupHasError.directive';
 export * from './directives/requiredClass/requiredClass.directive';
-export * from './services/submitted/submitted.service';
 export * from './services/errorMessagesExtractor/errorMessagesExtractor.interface';
 export * from './services/errorMessagesExtractor/errorMessagesExtractor.service';
+export * from './services/formModelBuilder/formModelBuilder.service';
+export * from './services/submitted/submitted.service';
 export * from './services/validationErrorRenderer/validationErrorRenderer.interface';
 export * from './services/validationErrorRenderer/validationErrorRenderer.service';
 export * from './misc/tokens';
