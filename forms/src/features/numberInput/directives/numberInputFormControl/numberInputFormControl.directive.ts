8cccfa09ce05248805c6296b138824d42fe5e355
kukjevov <kukjevov@gmail.com>
2026-07-29 12:56:56 +0200

    INT: fixed number input form control

--- forms/src/features/numberInput/directives/numberInputFormControl/numberInputFormControl.directive.ts
+++ forms/src/features/numberInput/directives/numberInputFormControl/numberInputFormControl.directive.ts
@@ -1,4 +1,4 @@
-import {computed, Directive, inject, input, InputSignal, model, ModelSignal, Signal} from '@angular/core';
+import {computed, Directive, ElementRef, inject, input, InputSignal, model, ModelSignal, signal, Signal, WritableSignal} from '@angular/core';
 import {FormValueControl} from '@angular/forms/signals';
 import {LOGGER, Logger} from '@anglr/common';
 import {isBlank} from '@jscrpt/common';
@@ -11,8 +11,8 @@ import {isBlank} from '@jscrpt/common';
     selector: 'input[number][formField]',
     host:
     {
-        '(change)': 'changeValue($event.target.value)',
-        '(input)': 'changeValue($event.target.value); touched.set(true)',
+        '(change)': 'changeValue($event.target)',
+        '(input)': 'changeValue($event.target); touched.set(true)',
         '(blur)': 'touched.set(true)',
         '[disabled]': 'disabled() || readonly()',
         '[value]': 'displayedValue()',
@@ -31,6 +31,11 @@ export class NumberInputFormControl implements FormValueControl<number|null|unde
      * Value that is displayed in input, it is used to prevent displaying invalid values in input
      */
     protected displayedValue: Signal<string>;
+
+    /**
+     * Internal value of form control, used for protecting input from changing value during change from input
+     */
+    protected internalValue: WritableSignal<number|null|undefined> = signal(undefined);
 
     //######################### public properties - implementation of FormValueControl #########################
 
@@ -60,13 +65,19 @@ export class NumberInputFormControl implements FormValueControl<number|null|unde
     public readonly touched: ModelSignal<boolean> = model(false);
 
     //######################### constructor #########################
-    constructor()
+    constructor(element: ElementRef<HTMLInputElement>)
     {
         this.displayedValue = computed(() =>
         {
             const value = this.value();
+            const internalValue = this.internalValue();
 
-            this.logger.verbose('NumberInput: Form control: setting input value "{{@(4)value}}"', {value});
+            if(value === internalValue || (isNaN(value ?? 0) && isNaN(internalValue ?? 0)))
+            {
+                return element.nativeElement.value;
+            }
+
+            this.logger.verbose('NumberInput: Form control: setting input value "{{value}}"', {value});
 
             if(isBlank(value) || isNaN(value))
             {
@@ -83,11 +94,19 @@ export class NumberInputFormControl implements FormValueControl<number|null|unde
      * Handles change of value in input
      * @param value - Changed value from input
      */
-    protected changeValue(value: string): void
+    protected changeValue(target: EventTarget|null): void
     {
+        if(!target)
+        {
+            throw new Error('NumberInput: Form control: target is null');
+        }
+
+        let value = (target as HTMLInputElement).value;
+
         if(isBlank(value) || value == '')
         {
             this.logger.verbose('NumberInput: Form control: setting control value to "null"');
+            this.internalValue.set(null);
             this.value.set(null);
 
             return;
@@ -98,13 +117,16 @@ export class NumberInputFormControl implements FormValueControl<number|null|unde
 
         if(!/^[+-]?\d+(?:[,.]\d+)?$/g.test(value))
         {
-            this.logger.verbose('NumberInput: Form control: setting control value to "NaN" because of invalid input "{{@(4)value}}"', {value});
+            this.logger.verbose('NumberInput: Form control: setting control value to "NaN" because of invalid input "{{value}}"', {value});
+            this.internalValue.set(NaN);
             this.value.set(NaN);
 
             return;
         }
 
-        this.logger.verbose('NumberInput: Form control: setting control value "{{@(4)value}}"', {value});
-        this.value.set(parseFloat(value.replace(',', '.')));
+        const parsedValue = parseFloat(value.replace(',', '.'));
+        this.logger.verbose('NumberInput: Form control: setting control value "{{parsedValue}}"', {parsedValue});
+        this.internalValue.set(parsedValue);
+        this.value.set(parsedValue);
     }
 }
